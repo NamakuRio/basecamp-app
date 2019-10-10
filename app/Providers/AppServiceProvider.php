ef9b90ac4dc6afb60bb3d33efe309be22addb90d
Rio Prastiawan <akunviprio@gmail.com>
2019-10-10 09:55:56 +0700

    set force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // force https if app debug false
+        if(config('app.debug')!=true) {
+            URL::forceScheme('https');
+        }
+
+        // default string length 191 character
         Schema::defaultStringLength(191);
     }
 }
