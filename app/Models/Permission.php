89c66753ceb1e95cb38eee82be95035432fb960e
Rio Prastiawan <akunviprio@gmail.com>
2019-10-10 12:44:24 +0700

    konfigurasi role permission

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -6,5 +6,50 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+
+    public function assignRole(...$roles)
+    {
+        $roles = $this->getAllRoles(Arr::flatten($roles));
+
+        if($roles == null) {
+            return $this;
+        }
+
+        $this->roles()->saveMany($roles);
+
+        return $this;
+
+    }
+
+    public function removeRole(... $roles)
+    {
+        $roles = $this->getAllRoles($roles);
+
+        $this->roles()->detach($roles);
+
+        return $this;
+    }
+
+    public function syncRoles(...$roles)
+    {
+        $this->roles()->detach();
+
+        return $this->assignRole($roles);
+    }
+
+    protected function getAllRoles(array $roles)
+    {
+        return Role::whereIn('name', $roles)->get();
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_permission');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_permission');
+    }
+
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,53 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Role extends Model
 {
-    //
+
+    public function givePermissionTo(...$permissions)
+    {
+        $permissions = $this->getAllPermissions(Arr::flatten($permissions));
+
+        if($permissions == null) {
+            return $this;
+        }
+
+        $this->permissions()->saveMany($permissions);
+
+        return $this;
+    }
+
+    public function revokePermissionTo(...$permissions)
+    {
+        $permissions = $this->getAllPermissions($permissions);
+
+        $this->permissions()->detach($permissions);
+
+        return $this;
+    }
+
+    public function updatePermissions(...$permissions)
+    {
+        $this->permissions()->detach();
+
+        return $this->givePermissionTo($permissions);
+    }
+
+    protected function getAllPermissions(array $permissions)
+    {
+        return Permission::whereIn('name', $permissions)->get();
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_role');
+    }
+
 }
